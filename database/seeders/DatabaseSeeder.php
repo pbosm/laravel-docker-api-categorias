002885a9e77f4d6e042a4a550548289f566eaaeb
pbosm <pablo.sm14@aluno.ifsc.edu.br>
2024-07-23 17:05:21 -0300

    fake dados users seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(1)->create();
+        \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'Test User1',
-            'email' => 'test1@example.com',
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
             'password' => Hash::make('123')
         ]);
 
